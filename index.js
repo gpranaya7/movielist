eafcd88879fcf8c160176261aeb0037d61944bec
unknown <gpranaya9@gmail.com>
2024-04-20 18:36:18 +0530

    complete app

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const films=[];
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/",(req,res)=>{
+  res.render("index.ejs",{films})
+
+});
+app.post("/submit",(req,res)=>{
+const {firstfilm}=req.body;
+films.push(firstfilm);
+  res.redirect("/");
+});
+
+
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+
+  
